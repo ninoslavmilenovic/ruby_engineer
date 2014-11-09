4e4986cfb3cbb9872d7808f81739cd524213ce98
pythogorian <nino.mil@gmail.com>
2014-11-09 17:44:17 +0100

    Add root permissions

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{www-data@104.236.19.199}
-role :web, %w{www-data@104.236.19.199}
-role :db,  %w{www-data@104.236.19.199}
+role :app, %w{root@104.236.19.199}
+role :web, %w{root@104.236.19.199}
+role :db,  %w{root@104.236.19.199}
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w{www-data@104.236.19.199}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '104.236.19.199', user: 'www-data', roles: %w{web app db}, my_property: :my_value
+server '104.236.19.199', user: 'root', roles: %w{web app db}, my_property: :my_value
 
 
 # Custom SSH Options
