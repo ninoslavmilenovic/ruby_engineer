ee0a5c327af70e8e76b609b00cd16c22ec1bbc6d
pythogorian <nino.mil@gmail.com>
2014-11-13 17:15:11 +0100

    Update server and deploy details

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :rbenv_ruby, '2.1.4'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/secrets.yml config/thin/production.yml}
+# set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -40,7 +40,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # execute "service thin restart"
+      # execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{root@104.236.19.199}
-role :web, %w{root@104.236.19.199}
-role :db,  %w{root@104.236.19.199}
+role :app, %w{deploy@rubyengineer.com}
+role :web, %w{deploy@rubyengineer.com}
+role :db,  %w{deploy@rubyengineer.com}
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w{root@104.236.19.199}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '104.236.19.199', user: 'root', roles: %w{web app db}, my_property: :my_value
+server 'rubyengineer.com', user: 'deploy', roles: %w{web app db}, my_property: :my_value
 
 
 # Custom SSH Options
