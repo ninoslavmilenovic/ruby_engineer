5bf93f670f326b5482316d148df60af8a2881da4
pythogorian <nino.mil@gmail.com>
2014-11-22 01:21:00 +0100

    Update Google Analytics

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -5,5 +5,7 @@
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-57005641-1', 'auto');
+  ga('require', 'displayfeatures');
+  ga('require', 'linkid', 'linkid.js');
   ga('send', 'pageview');
 });
