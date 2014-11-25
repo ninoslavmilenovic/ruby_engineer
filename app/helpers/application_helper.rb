1789b37aaeebe2cc0af253e9308e507dc03550f0
pythogorian <nino.mil@gmail.com>
2014-11-25 22:38:35 +0100

    Implement dynamic page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  def full_title(page_title="")
+    base_title = "Nino ● Ruby on Rails Engineer"
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
+
   def markdown(text)
     MARKDOWN.render(text).html_safe
   end
