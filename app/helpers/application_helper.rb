c09c606f532cc9e213e084df5dca5a51385ec3ae
pythogorian <nino.mil@gmail.com>
2014-12-24 00:11:48 +0100

    Update title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title="")
-    base_title = "Nino | Ruby on Rails Engineer"
+    base_title = "Nino ● Ruby on Rails Engineer"
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
 
